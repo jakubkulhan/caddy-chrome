1c43b5bcfeba6e4f5f73c74d62701b58b4f066ea
Jakub Kulhan <ahoj@jakubkulhan.cz>
2024-07-09 10:56:19 +0200

    parsing and provisioning of the middleware for remote browser

--- middleware.go
+++ middleware.go
@@ -9,6 +9,7 @@ import (
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	"github.com/chromedp/chromedp"
 	"go.uber.org/zap"
+	"strings"
 	"time"
 )
 
@@ -19,9 +20,21 @@ func init() {
 }
 
 type Middleware struct {
-	MIMETypes []string `json:"mime_types,omitempty"`
-	log       *zap.Logger
-	chromeCtx context.Context
+	MIMETypes     []string       `json:"mime_types,omitempty"`
+	ExecBrowser   *ExecBrowser   `json:"exec_browser,omitempty"`
+	RemoteBrowser *RemoteBrowser `json:"remote_browser,omitempty"`
+	log           *zap.Logger
+	chromeCtx     context.Context
+}
+
+type ExecBrowser struct {
+	Path         string   `json:"path,omitempty"`
+	DefaultFlags bool     `json:"default_flags,omitempty"`
+	Flags        []string `json:"flags,omitempty"`
+}
+
+type RemoteBrowser struct {
+	URL string `json:"url,omitempty"`
 }
 
 func (Middleware) CaddyModule() caddy.ModuleInfo {
@@ -36,10 +49,36 @@ func (m *Middleware) Provision(ctx caddy.Context) (err error) {
 		m.MIMETypes = []string{"text/html"}
 	}
 
+	if m.ExecBrowser == nil && m.RemoteBrowser == nil {
+		m.ExecBrowser = &ExecBrowser{DefaultFlags: true}
+	}
+	if m.ExecBrowser != nil && m.RemoteBrowser != nil {
+		return fmt.Errorf("cannot specify both exec and remote browser")
+	}
+
 	m.log = ctx.Logger()
 
 	var cancel context.CancelFunc
-	m.chromeCtx, cancel = chromedp.NewExecAllocator(context.Background(), chromedp.DefaultExecAllocatorOptions[:]...)
+	if m.ExecBrowser != nil {
+		var opts []chromedp.ExecAllocatorOption
+		if m.ExecBrowser.Path != "" {
+			opts = append(opts, chromedp.ExecPath(m.ExecBrowser.Path))
+		}
+		if m.ExecBrowser.DefaultFlags {
+			opts = append(opts, chromedp.DefaultExecAllocatorOptions[:]...)
+		}
+		for _, flag := range m.ExecBrowser.Flags {
+			parts := strings.SplitN(flag, "=", 2)
+			opts = append(opts, chromedp.Flag(parts[0], parts[1]))
+		}
+		m.chromeCtx, cancel = chromedp.NewExecAllocator(context.Background(), opts...)
+
+	} else if m.RemoteBrowser != nil {
+		m.chromeCtx, cancel = chromedp.NewRemoteAllocator(context.Background(), m.RemoteBrowser.URL)
+
+	} else {
+		panic("unreachable")
+	}
 	m.chromeCtx, _ = chromedp.NewContext(m.chromeCtx)
 	defer func() {
 		if err != nil {
@@ -67,13 +106,6 @@ func (m *Middleware) Cleanup() error {
 	return nil
 }
 
-func (m *Middleware) Validate() error {
-	if len(m.MIMETypes) == 0 {
-		return fmt.Errorf("mime_types must not be empty")
-	}
-	return nil
-}
-
 func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
 	m := &Middleware{}
 	err := m.UnmarshalCaddyfile(h.Dispenser)
@@ -85,13 +117,43 @@ func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error)
 
 func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
-		for d.NextBlock(0) {
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			defaultFlags := true
 			switch d.Val() {
 			case "mime_types":
 				m.MIMETypes = d.RemainingArgs()
 				if len(m.MIMETypes) == 0 {
 					return d.ArgErr()
 				}
+			case "exec_no_default_flags":
+				defaultFlags = false
+				fallthrough
+			case "exec":
+				m.ExecBrowser = &ExecBrowser{
+					DefaultFlags: defaultFlags,
+				}
+				flags := false
+				for d.NextArg() {
+					if strings.HasPrefix(d.Val(), "--") {
+						flags = true
+					}
+					if d.Val() == "--" {
+						continue
+					} else if flags {
+						m.ExecBrowser.Flags = append(m.ExecBrowser.Flags, d.Val())
+					} else {
+						m.ExecBrowser.Path = d.Val()
+					}
+				}
+			case "url":
+				m.RemoteBrowser = &RemoteBrowser{}
+				if d.CountRemainingArgs() != 1 {
+					return d.ArgErr()
+				}
+				d.NextArg()
+				m.RemoteBrowser.URL = d.Val()
+			default:
+				return d.ArgErr()
 			}
 		}
 	}
@@ -102,6 +164,5 @@ var (
 	_ caddy.Module          = (*Middleware)(nil)
 	_ caddy.Provisioner     = (*Middleware)(nil)
 	_ caddy.CleanerUpper    = (*Middleware)(nil)
-	_ caddy.Validator       = (*Middleware)(nil)
 	_ caddyfile.Unmarshaler = (*Middleware)(nil)
 )
---
+++ middleware_test.go
@@ -0,0 +1,106 @@
+package caddy_chrome
+
+import (
+	"encoding/json"
+	"github.com/alecthomas/assert/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"regexp"
+	"testing"
+)
+
+func TestMiddleware_UnmarshalCaddyfile(t *testing.T) {
+	re := regexp.MustCompile(`\s+`)
+	for _, testCase := range []struct {
+		caddyfile string
+		json      string
+	}{
+		{
+			caddyfile: `chrome`,
+			json:      `{}`,
+		},
+		{
+			caddyfile: `chrome {
+			}`,
+			json: `{}`,
+		},
+		{
+			caddyfile: `chrome {
+				mime_types text/html
+			}`,
+			json: `{"mime_types":["text/html"]}`,
+		},
+		{
+			caddyfile: `chrome {
+				mime_types text/html application/xhtml+xml
+			}`,
+			json: `{"mime_types":["text/html","application/xhtml+xml"]}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec
+			}`,
+			json: `{"exec_browser":{"default_flags":true}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec /usr/bin/chrome
+			}`,
+			json: `{"exec_browser":{"path":"/usr/bin/chrome","default_flags":true}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec /usr/bin/chrome --
+			}`,
+			json: `{"exec_browser":{"path":"/usr/bin/chrome","default_flags":true}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec /usr/bin/chrome --headless
+			}`,
+			json: `{"exec_browser":{"path":"/usr/bin/chrome","default_flags":true,"flags":["--headless"]}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec /usr/bin/chrome -- --headless
+			}`,
+			json: `{"exec_browser":{"path":"/usr/bin/chrome","default_flags":true,"flags":["--headless"]}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec --headless
+			}`,
+			json: `{"exec_browser":{"default_flags":true,"flags":["--headless"]}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec_no_default_flags /usr/bin/chrome
+			}`,
+			json: `{"exec_browser":{"path":"/usr/bin/chrome"}}`,
+		},
+		{
+			caddyfile: `chrome {
+				exec_no_default_flags
+			}`,
+			json: `{"exec_browser":{}}`,
+		},
+		{
+			caddyfile: `chrome {
+				url http://localhost:9222/
+			}`,
+			json: `{"remote_browser":{"url":"http://localhost:9222/"}}`,
+		},
+	} {
+		t.Run(re.ReplaceAllString(testCase.caddyfile, " "), func(t *testing.T) {
+			m := new(Middleware)
+			err := m.UnmarshalCaddyfile(caddyfile.NewTestDispenser(testCase.caddyfile))
+			if err != nil {
+				t.Fatal(err)
+			}
+			j, err := json.Marshal(m)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, testCase.json, string(j))
+		})
+	}
+}
